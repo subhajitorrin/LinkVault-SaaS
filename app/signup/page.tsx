1774537450fb33b21ed7e48a290f8dd03e9a8317
subhajitorrin <subhajitghosh343@gmail.com>
2024-10-31 03:35:28 +0530

    update .eslintrc.json and page.tsx

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -6,13 +6,38 @@ import { Link } from "next/link";
 
 const Signup = () => {
   const { isLoaded, signUp, setActive } = useSignUp();
-  const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [emailAddress, setEmailAddress] = useState<string>("");
   const [pendingVerify, setPendingVerify] = useState<boolean>(false);
   const [otp, setOtp] = useState<string>("");
   const [error, seterror] = useState("");
   const [togglePass, setTogglePass] = useState<boolean>(false);
   const router = useRouter();
+
+  if (!isLoaded) {
+    return null;
+  }
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    if (!isLoaded) {
+      return;
+    }
+    try {
+      await signUp.create({
+        emailAddress,
+        password
+      });
+      await signUp.prepareEmailAddressVerification({
+        strategy: "email_code"
+      });
+      setPendingVerify(true);
+    } catch (error: any) {
+      console.error(error);
+      seterror(error.errors[0].message);
+    }
+  }
+
   return (
     <div>
       Signup <Button variant={"outline"}>Click me</Button>
