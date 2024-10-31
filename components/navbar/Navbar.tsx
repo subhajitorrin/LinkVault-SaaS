2da746f58427c41c0553aee4a9e80a81a4c6f71c
subhajitorrin <subhajitghosh343@gmail.com>
2024-10-31 10:52:41 +0530

    nav mobile

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,8 +1,15 @@
+"use client";
 import { ExitIcon } from "@radix-ui/react-icons";
-import { Compass, DollarSign, Home } from "lucide-react";
+import { Compass, DollarSign, Home, Menu } from "lucide-react";
 import Link from "next/link";
 import React from "react";
-import { Button } from "../ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger
+} from "@/components/ui/sheet";
 
 const navItems = [
   {
@@ -23,12 +30,13 @@ const navItems = [
 ];
 
 const Navbar = () => {
+  const [open, setOpen] = React.useState<boolean>(false);
   return (
-    <nav className="py-4 px-[10%] w-full flex justify-between text-white items-center">
-      <Link href={"/"} className="font-bold text-2xl">
+    <nav className="py-4 md:px-[10%] px-[5%] w-full flex justify-between text-white items-center">
+      <Link href={"/"} className="font-bold md:text-2xl text-lg">
         LinksVault
       </Link>
-      <div className="flex items-center gap-3 bg-zinc-900 rounded-lg px-2 py-1">
+      <div className="md:flex hidden items-center gap-3 bg-zinc-900 rounded-lg px-2 py-1">
         {navItems.map((item, index) => {
           return (
             <Link
@@ -42,12 +50,43 @@ const Navbar = () => {
           );
         })}
       </div>
-      <Link
-        href={"/signup"}
-        className=" font-medium flex items-center gap-2 text-sm"
-      >
-        <ExitIcon className="h-4 w-4" /> <p>Login</p>
-      </Link>
+      <div className="flex items-center gap-4">
+        <Link
+          href={"/signup"}
+          className=" font-medium flex items-center gap-2 text-sm"
+        >
+          <ExitIcon className="h-4 w-4 md:block hidden" /> <p>Login</p>
+        </Link>
+        <Sheet open={open} onOpenChange={setOpen}>
+          <SheetTrigger asChild>
+            <Menu className="md:hidden cursor-pointer h-5 w-5" />
+          </SheetTrigger>
+          <SheetContent
+            side="right"
+            className="w-[300px] sm:w-[400px] bg-black/80 backdrop-blur-md border-r border-gray-800"
+          >
+            <SheetHeader className="hidden">
+              <SheetTitle className="text-2xl font-bold text-white">
+                Navigation
+              </SheetTitle>
+            </SheetHeader>
+            <nav className="flex flex-col space-y-4 mt-8 text-center">
+              {navItems.map((item, index) => {
+                return (
+                  <Link
+                    key={index}
+                    href={item.path}
+                    onClick={() => setOpen(false)}
+                    className="text-base text-gray-300 hover:text-white transition-colors duration-200 ease-in-out"
+                  >
+                    {item.name}
+                  </Link>
+                );
+              })}
+            </nav>
+          </SheetContent>
+        </Sheet>
+      </div>
     </nav>
   );
 };
