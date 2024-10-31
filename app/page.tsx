18f15f2413a58a176a3ee58ef28e724e8c987ff4
subhajitorrin <subhajitghosh343@gmail.com>
2024-10-31 11:09:42 +0530

    update page.tsx, globals.css and page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const Home = () => {
+const LandingPage = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="h-screen text-white">
+      {/* herosection */}
+      <section className="mt-16">
+        <h1 className="md:text-center text-start text-gradient leading-tight md:text-7xl md:font-medium">
+          Streamline Your Links
+        </h1>
+        <p className="md:text-sm opacity-50 text-center mt-4">
+          Effortlessly organize and categorize your links with AI-driven
+          precision, <br /> making access and management simple and efficient.
+        </p>
+      </section>
+    </div>
+  );
+};
 
-export default Home
+export default LandingPage;
