d791c2d047978154d5dd1b36216db22ab0bf20e7
subhajitorrin <subhajitghosh343@gmail.com>
2024-10-31 12:08:39 +0530

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,25 +5,26 @@ import React from "react";
 
 const LandingPage = () => {
   return (
-    <div className="h-screen text-white">
+    <div className="h-screen text-white px-[5%] md:px-[10%]">
       {/* herosection */}
       <section className="mt-16">
-        <h1 className="md:text-center text-start text-gradient leading-tight md:text-7xl md:font-semibold">
+        <h1 className="md:text-center text-start text-gradient leading-tight md:text-7xl text-3xl font-semibold">
           Streamline Your Links
         </h1>
-        <p className="md:text-sm opacity-50 text-center mt-4">
+        <p className="md:text-sm text-xs opacity-50 md:text-center text-start md:mt-4">
           Effortlessly organize and categorize your links with AI-driven
-          precision, <br /> making access and management simple and efficient.
+          precision, <br className="md:block hidden" /> making access and
+          management simple and efficient.
         </p>
-        <div className="flex md:flex-row flex-col md:justify-center gap-5 md:mt-5 w-full">
+        <div className="flex items-center md:flex-row flex-col md:justify-center gap-2 md:gap-5 mt-5 w-full">
           <Button
             variant="outline"
-            className="rounded-lg bg-transparent text-sm"
+            className="rounded-md bg-transparent text-sm w-full md:w-auto border border-[#1e1e1e]"
           >
             Watch Demo
           </Button>
-          <Link href="/signin">
-            <Button className="rounded-lg text-sm flex gap-2 w-full">
+          <Link href="/signin" className="w-full md:w-auto">
+            <Button className="rounded-md text-sm flex gap-2 w-full">
               <BadgePlus /> Get Started
             </Button>
           </Link>
