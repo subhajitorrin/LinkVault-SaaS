999d90cadf7c346033d0bd5967d681528692d7e1
subhajitorrin <subhajitghosh343@gmail.com>
2024-10-31 12:47:16 +0530

    update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className="bg-zinc-950">
+        <body className={` bg-zinc-950`}>
           <Navbar />
           {children}
         </body>
