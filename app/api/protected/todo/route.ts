42e448ff405848f7a13382954b581efbe208d91c
subhajitorrin <subhajitghosh343@gmail.com>
2024-11-01 17:49:02 +0530

    update route.ts

--- app/api/protected/todo/route.ts
+++ app/api/protected/todo/route.ts
@@ -2,9 +2,15 @@ import prisma from "@/lib/prisma";
 
 export async function POST(req: Request) {
     const payload = await req.json();
-    console.log(payload);
-    return new Response("OK", { status: 200 });
+    if (!payload.url) {
+        return new Response("Invalid url", { status: 400 });
+    }
+    try {
+        await prisma.todo.create({ link: payload.url, title: payload.title });
+        return new Response("Created", { status: 200 });
+    } catch (error) {
+        console.log(error);
+        return new Response("Internal server error", { status: 400 });
+    }
 }
-export async function GET(req: Request) {
-    return new Response("OK", { status: 200 });
-}
+
